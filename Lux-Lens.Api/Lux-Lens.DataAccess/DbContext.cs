0296909442789d2f388ab953b9c9bba28e5ef82b
Iveth Mariana Serna <mariana.serna@puntosingular.com>
2023-08-18 13:46:35 -0600

    Update Project structure

---
+++ Lux-Lens.Api/Lux-Lens.DataAccess/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Lux_Lens.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lux_Lens.DataAccess
+{
+    public class DbContext : IdentityDbContext
+    {
+        public virtual DbSet<Lens> Lens { get; set; }
+        public DbContext(DbContextOptions <DbContext> options) : base(options)
+        {
+
+        }
+    }
+}
