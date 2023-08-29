95ffbee913134e8932a4143b3ffedb0bd4ce11b2
BNN0 <76565186+BNN0@users.noreply.github.com>
2023-08-29 15:44:04 -0600

    UnitTest

---
+++ Lux-Lens.Api/Lux-Lens.UnitTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lux_Lens.UnitTest
+{
+    public class Startup
+    {
+        
+    }
+}
