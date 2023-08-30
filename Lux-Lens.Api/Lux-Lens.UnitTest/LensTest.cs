5d1f255ac7882463ad0a2f4e3e4990dcdf767e4f
BNN0 <76565186+BNN0@users.noreply.github.com>
2023-08-30 17:10:30 -0600

    UnitTest Update
    Correction in methods of LensTest: GetAll and GetById

--- Lux-Lens.Api/Lux-Lens.UnitTest/LensTest.cs
+++ Lux-Lens.Api/Lux-Lens.UnitTest/LensTest.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Castle.Components.DictionaryAdapter.Xml;
 using Lux_Lens.ApplicationServices.Lens;
 using Lux_Lens.AppServices.Shared.Dto;
 using Lux_Lens.Core.Entities;
 using LuxLens.Api.Controllers.LensControllers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
@@ -73,7 +76,9 @@ namespace Lux_Lens.UnitTest
         public void GetByIdTest()
         {
             // Arrange
-            var lensDto = new LensDto
+            var lensId = 1;
+
+            var lensDto = new Core.Entities.Lens
             {
                 Cantidad = 100,
                 Precio = 100,
@@ -83,55 +88,54 @@ namespace Lux_Lens.UnitTest
                 Material = "MaterialTest"
             };
 
-            //Act
-           _LensController.Create(lensDto); 
-            var result = _LensController.GetById(1);
+            _LensServiceMock.Setup(service => service.GetLensByIdAsync(lensId))
+                        .ReturnsAsync(lensDto);
+
+            // Act
+            var result = _LensController.GetById(lensId);
 
             // Assert
-
-            var a = result.Result;
-            Assert.IsNotNull(a);
-            Assert.That(lensDto.Precio, Is.EqualTo(a.Precio));
+            // Assert
+            var lens = result.Result;
+            Assert.NotNull(lens);
+            Assert.That(lensDto, Is.EqualTo(lens));
         }
 
         [Test]
         public void GetAllByIdTest()
         {
             // Arrange
-            var lensDto = new LensDto
+            var lens = new List<Core.Entities.Lens>
             {
-                Cantidad = 100,
-                Precio = 100,
-                Marca = "MarcaTest",
-                Modelo = "ModeloTest",
-                Color = "ColorTest",
-                Material = "MaterialTest"
+                new Core.Entities.Lens
+                {
+                    Cantidad = 100,
+                    Precio = 100,
+                    Marca = "MarcaTest",
+                    Modelo = "ModeloTest",
+                    Color = "ColorTest",
+                    Material = "MaterialTest"
+                },
+                new Core.Entities.Lens
+                {
+                    Cantidad = 200,
+                    Precio = 200,
+                    Marca = "MarcaTest2",
+                    Modelo = "ModeloTest2",
+                    Color = "ColorTest2",
+                    Material = "MaterialTest2"
+                },
             };
 
+            _LensServiceMock.Setup(service => service.GetLensAsync()).ReturnsAsync(lens);
+
             // Act
-            _LensController.Create(lensDto);
             var result = _LensController.GetAllLenses();
 
             // Assert
-            Assert.IsNotNull(result);
-
-            // Ensure the result is of type ActionResult<List<Lens>>
-            var resultOk = result.Result;
-            Assert.IsNotNull(resultOk[1]);
-
-            // Ensure the value in the result is a list of lenses
-            Assert.IsInstanceOf<List<Lens>>(resultOk);
-            var obtainedLenses = resultOk ;
-            Assert.AreEqual(1, obtainedLenses.Count); // There should be one item in the list
-
-            // Compare the properties of the obtained lens with the expected lensDto
-            var obtainedLens = obtainedLenses[0];
-            Assert.AreEqual(lensDto.Cantidad, obtainedLens.Cantidad);
-            Assert.AreEqual(lensDto.Precio, obtainedLens.Precio);
-            Assert.AreEqual(lensDto.Marca, obtainedLens.Marca);
-            Assert.AreEqual(lensDto.Modelo, obtainedLens.Modelo);
-            Assert.AreEqual(lensDto.Color, obtainedLens.Color);
-            Assert.AreEqual(lensDto.Material, obtainedLens.Material);
+            var lenses = result.Result;
+            Assert.NotNull(lenses);
+            Assert.That(lens, Is.EqualTo(lenses));
         }
 
 
